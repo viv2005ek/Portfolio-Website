17f0ab4448be973787274697178b42154583ce97
Vivek Kumar Garg <142710112+viv2005ek@users.noreply.github.com>
2025-10-20 19:05:30 +0530

    Update follower count in portfolio.ts

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -7,7 +7,7 @@ export const personalInfo = {
   linkedin: "https://www.linkedin.com/in/vivek-kumar-garg-097677280/",
   X:"https://x.com/viv2005ek",
   github: "https://github.com/viv2005ek",
-  followers: "2.3k+",
+  followers: "3k+",
   bio: "Computer Science undergraduate with strong full-stack development skills, leadership experience, startup exposure, with 2+ years of hands-on project and internship experience. Built and deployed scalable web applications; mentored open-source contributors across multiple tech communities.",
   cgpa: 9.46,
   university: "Manipal University Jaipur",
